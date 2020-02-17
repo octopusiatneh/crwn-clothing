510628afd7ddcc64da256bcd767bb29de36ab52c
octopusiatneh <ntnq1910@gmail.com>
2020-02-17 12:40:17 +0700

    add collectionSelector, update ux flow

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,64 +1,25 @@
 import React, { Component } from "react";
-import "./directory.style.scss";
+
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import { selectDirectorySections } from "../../redux/directory/directory.selectors";
+
 import MenuItem from "../menu-item/menu-item.component";
 
-class Directory extends Component {
-  constructor() {
-    super();
+import "./directory.style.scss";
 
-    this.state = {
-      categories: [
-        {
-          title: "hats",
-          imageUrl:
-            "https://images.unsplash.com/photo-1525740615880-50bb88fe0b1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-          id: 1,
-          linkUrl: "hats"
-        },
-        {
-          title: "jackets",
-          imageUrl:
-            "https://images.unsplash.com/photo-1521223890158-f9f7c3d5d504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=628&q=80",
-          id: 2,
-          linkUrl: ""
-        },
-        {
-          title: "sneakers",
-          imageUrl:
-            "https://images.unsplash.com/photo-1524010349062-860def6649c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-          id: 3,
-          linkUrl: ""
-        },
-        {
-          title: "womens",
-          imageUrl:
-            "https://images.unsplash.com/photo-1504703395950-b89145a5425b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=651&q=80",
-          id: 4,
-          size: "large",
-          linkUrl: ""
-        },
-        {
-          title: "mens",
-          imageUrl:
-            "https://images.unsplash.com/photo-1521341057461-6eb5f40b07ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-          id: 5,
-          size: "large",
-          linkUrl: ""
-        }
-      ]
-    };
-  }
+const Directory = ({ sections }) => (
+  <div className="directory-menu">
+    {//destructoring category
+    sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </div>
+);
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {//destructoring category
-        this.state.categories.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+});
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,50 @@
+const INITIAL_STATE = {
+  sections: [
+    {
+      title: "hats",
+      imageUrl:
+        "https://images.unsplash.com/photo-1525740615880-50bb88fe0b1f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      id: 1,
+      linkUrl: "shop/hats"
+    },
+    {
+      title: "jackets",
+      imageUrl:
+        "https://images.unsplash.com/photo-1521223890158-f9f7c3d5d504?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=628&q=80",
+      id: 2,
+      linkUrl: "shop/jackets"
+    },
+    {
+      title: "sneakers",
+      imageUrl:
+        "https://images.unsplash.com/photo-1524010349062-860def6649c3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      id: 3,
+      linkUrl: "shop/sneakers"
+    },
+    {
+      title: "womens",
+      imageUrl:
+        "https://images.unsplash.com/photo-1504703395950-b89145a5425b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=651&q=80",
+      id: 4,
+      size: "large",
+      linkUrl: "shop/womens"
+    },
+    {
+      title: "mens",
+      imageUrl:
+        "https://images.unsplash.com/photo-1521341057461-6eb5f40b07ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
+      id: 5,
+      size: "large",
+      linkUrl: "shop/mens"
+    }
+  ]
+};
+
+const directoryReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default directoryReducer;
